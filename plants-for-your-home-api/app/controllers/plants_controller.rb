bd22b983bd35b9b724ca7349b32659ad61370686
Richard <richardkangg@gmail.com>
2020-01-13 10:38:32 -0500

    delete controller

--- plants-for-your-home-api/app/controllers/plants_controller.rb
+++ plants-for-your-home-api/app/controllers/plants_controller.rb
@@ -6,5 +6,16 @@ class PlantsController < ApplicationController
     def show
         plant = Plant.find(params[:id])
         render json: { status: 200, plant: plant }
-      end
+    end
+
+    def update
+        update_plant = Plant.update(params[:id])
+        render json: { status: 200, book: update_plant }
+    end
+
+    def destroy
+        destroy_plant = Plant.destroy(params[:id])
+        render json: { status: 200, book: destroy_plant }
+    end
+
 end
